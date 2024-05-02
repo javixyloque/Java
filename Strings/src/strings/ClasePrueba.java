0f0d2c65d26b758eb36a301b4430a5329ddd163d
javixyloque <perosa1000@gmail.com>
2024-05-02 10:48:00 +0200

    primera

---
+++ Strings/src/strings/ClasePrueba.java
@@ -0,0 +1,10 @@
+package strings;
+
+public class ClasePrueba {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Esta es la primera versión");
+	}
+
+}
