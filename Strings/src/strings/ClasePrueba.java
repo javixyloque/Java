d283de2e3272b156b1008bc841e8f68a7fd31453
javixyloque <perosa1000@gmail.com>
2024-05-02 10:49:25 +0200

    segunda

--- Strings/src/strings/ClasePrueba.java
+++ Strings/src/strings/ClasePrueba.java
@@ -4,7 +4,7 @@ public class ClasePrueba {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Esta es la primera versión");
+		System.out.println("Esta es la segunda versión");
 	}
 
 }
