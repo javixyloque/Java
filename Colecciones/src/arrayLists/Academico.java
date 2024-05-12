615d6bb129dd22582e3ed38a582455baf153a708
javixyloque <javixyloque@Javi-Katana>
2024-05-12 23:59:52 +0200

    12 mayo trabajo casa

---
+++ Colecciones/src/arrayLists/Academico.java
@@ -0,0 +1,34 @@
+package arrayLists;
+
+public class Academico {
+	private String nombre;
+	private int aIngreso;
+	
+	public Academico(String nombre, int aIngreso) {
+		super();
+		this.nombre = nombre;
+		this.aIngreso = aIngreso;
+	}
+
+	@Override
+	public String toString() {
+		return "Academico [nombre=" + nombre + ", aIngreso=" + aIngreso + "]";
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getaIngreso() {
+		return aIngreso;
+	}
+
+	public void setaIngreso(int aIngreso) {
+		this.aIngreso = aIngreso;
+	}
+	
+}
---
+++ Colecciones/src/arrayLists/PrincipalAcademico.java
@@ -0,0 +1,33 @@
+package arrayLists;
+
+import java.util.*;
+
+public class PrincipalAcademico {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Academico a = new Academico ("Joselu", 2006);
+		Character letra = 'F';
+		HashMap<Character, Academico> mapa = new HashMap();
+		nuevoAcademico(mapa, a, letra);
+		System.out.println(nuevoAcademico(mapa, a, letra));
+	}
+
+	static boolean nuevoAcademico (HashMap<Character, Academico> mapa, Academico a, Character letra) {
+	
+		if ((65<=letra && letra<=90 ) || (97<=letra && letra<=122)) {
+			mapa.put(letra, a);
+		}
+
+		System.out.println(mapa);
+		if (mapa.containsValue(a)) {
+			return true;
+		} else {
+			return false;
+		}
+		
+		
+		
+	}
+	
+}
